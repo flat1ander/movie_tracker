f4055633e67a00b63d1aa4772ae24de8844d561f
flat1ander <thematthewlane@gmail.com>
2023-07-15 11:23:39 -0400

    Trying to get static files to deploy properly

--- movie_tracker/settings.py
+++ movie_tracker/settings.py
@@ -88,31 +88,31 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres.
 
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'movie_tracker',
-    'USER': os.environ['DB_USER'],
-    'PASSWORD': os.environ['DB_PW'],
-    'HOST': os.environ['DB_HOST'],
-    'PORT': '5432',
-    'OPTIONS': {
-            'sslmode': 'require',
-        },
-  }
-}
-
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'movie_tracker',
-#         **dj_database_url.config(
-#             conn_max_age=600,
-#             conn_health_checks=True,
-#         ),
-#     }
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'movie_tracker',
+#     'USER': os.environ['DB_USER'],
+#     'PASSWORD': os.environ['DB_PW'],
+#     'HOST': os.environ['DB_HOST'],
+#     'PORT': '5432',
+#     'OPTIONS': {
+#             'sslmode': 'require',
+#         },
+#   }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movie_tracker',
+        **dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+        ),
+    }
+}
 
 
 
