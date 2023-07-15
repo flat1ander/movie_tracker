16dfa129fa9dcff58456e6b73c8cc1d658b41c22
flat1ander <thematthewlane@gmail.com>
2023-07-15 11:30:21 -0400

    Trying to get static files to deploy properly

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ source /opt/render/project/src/.venv/bin/activate
 pip3 install -r deps.txt
 
 # Run the build commands
-python3 manage.py collectstatic --no-input
+python3 manage.py collectstatic
 python3 manage.py migrate
 
